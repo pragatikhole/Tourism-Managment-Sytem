0f74db309eb15c225ef3d9ba823dada610955969
pragatikhole <100992708+pragatikhole@users.noreply.github.com>
2022-03-05 14:38:57 +0530

    Add files via upload

---
+++ slider.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<!---banner--->
+<!----start-slider-script---->
+			<script src="js/responsiveslides.min.js"></script>
+			<!----//End-slider-script---->
+			<!-- Slideshow 4 -->
+			<div id="section-1" class="section">
+			    <div id="top" class="callbacks_container">
+			      <ul class="rslides" id="slider4">
+			        <li>
+			          <img src="images/img5.jpg" alt="">
+					  <div class="caption">
+			     	  		<div class="header-info">
+							<h2><a href="#">Get Away On This Weekend</a></h2>
+							<lable></lable>
+							<h1><a href="#">HEAVEN BEACH RESORT</a></h1>
+							</div>
+			          </div>
+			        </li>
+			        
+			      </ul>
+			    </div>	         
+			    <div class="clearfix"> </div>
+				</div>
+		<!----- //End-slider---->
+<!---banner--->	
+</body>
+</html>
